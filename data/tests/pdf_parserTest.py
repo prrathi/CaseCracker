d8202039633b629f6d653880fe2fb44ff54e59d4
Praneet Rathi <prrathi10@gmail.com>
2022-03-09 16:03:11 -0600

    add format for test and update organization for pdf_parser.py

--- data/tests/pdf_parserTest.py
+++ data/tests/pdf_parserTest.py
@@ -1,18 +1,4 @@
 import pytest
 import sys
 sys.path.insert(1, "./data/nlp/")
-import classify
-
-# Unit test for findIssueAvg() function in classify.py
-def test_findIssueAvg():
-    responses = [1, 1.5, 2, 1, 1, 1, 2, 2, 2, 1, 1.2, 1.4, 2, 1.5, 1.75, 1.5, 1.25, 1, 1, 1, 1, 2, 2, 2, 1.5, 1.5, 1.5, 1.1, 1.0, 1.2, 1.5, 1.6, 1.7, 2, 2, 2, 1.2, 1.4, 1.6, 1.5, 1, 2]
-    print (len(responses))
-    assert classify.findIssueAvg(responses) == [1.5, 1.0, 2.0, 1.2, 1.75, 1.25, 1.0, 2.0, 1.5, 1.1, 1.6, 2.0, 1.4, 1.5 ]
-
-# Unit Test for distance()
-def test_distance():
-    responses = [1, 1.5, 2, 1, 1, 1, 2, 2, 2, 1, 1.2, 1.4, 2, 1.5, 1.75, 1.5, 1.25, 1, 1, 1, 1, 2, 2, 2, 1.5, 1.5, 1.5, 1.1, 1.0, 1.2, 1.5, 1.6, 1.7, 2, 2, 2, 1.2, 1.4, 1.6, 1.5, 1, 2]
-    responses = classify.findIssueAvg(responses)
-    targetJustice = [1, 1.5, 2, 2, 1.4, 1.9, 1.5, 1, 2, 1.5, 1, 2, 1.3, 1.4]
-    assert classify.distance(responses, targetJustice) == 0.266
-
+import pdf_parser
