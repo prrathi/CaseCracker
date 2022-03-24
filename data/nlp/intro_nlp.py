e7717db3ab712482451e6294640ec1996ee85149
Praneet Rathi <prrathi10@gmail.com>
2022-03-24 17:38:07 -0500

    update the transcript files and add to the intro_nlp script

--- data/nlp/intro_nlp.py
+++ data/nlp/intro_nlp.py
@@ -1,44 +1,81 @@
+from operator import index
+from bleach import clean
 import pandas as pd
+import numpy as np
 import time
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 stopwords.words('english')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from datetime import date
-import gensim
+import re
+from gensim import models
 from gensim.parsing.preprocessing import preprocess_documents
 
 
-vader = SentimentIntensityAnalyzer()
-df = pd.read_csv("sample.csv") #TODO: organize loaded data as dataframes with text composing one of columns
+df = pd.read_csv("transcripts_new.csv", encoding = 'unicode_escape', engine ='python')
+# df.dropna(how = 'all', inplace=True)
+# df.to_csv("transcripts.csv", index=False)
 
-columns = ["NaiveSentiment", "LearnedSentiment"] #ADD MORE LATER
-dfOutput = pd.DataFrame(columns=columns)
+dfClean = pd.DataFrame(index=range(df.shape[0]), columns = df.columns)
+dfClean["Year"] = df["Year"]
+dfClean["ID"] = df["ID"]
+
+columns = ["clean", "LearnedSentiment"] #ADD MORE LATER
+dfOutput = pd.DataFrame(index=range(df.shape[0]), columns=columns)
 
 # get naive polarity score using the sentiment analysis tool from nltk
 # run the analysis of texts for each entry in the columm, this gives us overall sentiment for each speech given 
-naive = []
-corpus = df["Sample2"].values
-for text in df["Sample2"]: # change to actual column name
 
-    naive.append(vader.polarity_scores(text)['compound'])
+def cleanText(df, col):
+    finalText = []
+    for text in df[col]: # change to actual column name
 
-    split = text.split(' ')
-    tokens = [t for t in split]
-    clean_tokens = tokens[:]
+        split = text.split(' ')
+        tokens = [t for t in split]
+        clean_tokens = []
 
-    for token in clean_tokens:
-        if token in stopwords.words('english'):
-            clean_tokens.remove(token)
-    # remove stop words for future use in other nlp modeling
+        for i in range(len(tokens)):
+            token = tokens[i]
+            if re.search('[a-zA-Z]', token) and token not in stopwords.words('english'):
+                token = re.sub(' +', ' ', token)
+                clean_tokens.append(token)
 
-processed_corpus = preprocess_documents(corpus)
-dict_corpus = [gensim.corpora.Dictionary(processed_corpus).doc2bow(text) for text in processed_corpus]
-# preprocessing for the corpus
+        finalText.append(' '.join(clean_tokens))
+    return finalText
 
-tfidf = gensim.models.TfidfModel(dict_corpus, smartirs='nfc')
-tfidf_corpus = tfidf[dict_corpus]
+def polarityScore(df, cols):
+    vader = SentimentIntensityAnalyzer()
+    polarity = []
+    for i in range(df.shape[0]):
+        polarity.append(vader.polarity_scores(df[cols[1]][i])['compound'] - vader.polarity_scores(df[cols[0]][i])['compound'])
+    return polarity
 
+def convertCorpus(df, col):
+    corpus = df[col].values
+    processed_corpus = preprocess_documents(corpus)
+    dict_corpus = [gensim.corpora.Dictionary(processed_corpus).doc2bow(text) for text in processed_corpus]
+    tfidf = gensim.models.TfidfModel(dict_corpus, smartirs='nfc')
+    tfidf_corpus = tfidf[dict_corpus]
+    return tfidf_corpus
+
+cols = ["Petitioner", "Respondent"]
+for col in cols:
+    cleanedCol = cleanText(df, col)
+    dfClean[col] = pd.Series(cleanedCol)
+
+polarityCol = polarityScore(dfClean, cols)
+dfOutput[col] = pd.Series(polarityCol)
+
+petCorpus = convertCorpus(dfClean, "Petitioner")
+resCorpus = convertCorpus(dfClean, "Respondent")
+
+model = models.LsiModel(petCorpus, num_topics = 50)
+print(model[petCorpus[0]])
+
+dfClean.to_csv("transcripts_clean.csv", index=False)
+
+"""
 for num_topic in range(50, 1000, 50):
     print(num_topic)
     lsi = gensim.models.LsiModel(tfidf_corpus, num_topics=num_topic)
@@ -54,4 +91,4 @@ for num_topic in range(50, 1000, 50):
         # print(s[1])
     # for text in df["Sample2"]:
         # print(lsi[text])
-
+"""
