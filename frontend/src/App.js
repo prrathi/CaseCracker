63a31e1d4765c4d65aa26b2c4960584e02d2e2e5
Aryan Nambiar <aryanjnambiar@gmail.com>
2022-03-29 15:52:35 -0500

    Initial Justices commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Justices from './components/pages/Justices';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route> 
         <Route path='/About' element={<About />}></Route> 
+        <Route path='/Justices' element={<Justices />}></Route> 
       </Routes>
       </Router>
 
