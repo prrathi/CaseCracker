5bdf9fada0f081ec84aa23dce68ef32b119b0f7e
Praneet Rathi <prrathi10@gmail.com>
2022-03-30 08:38:46 -0500

    get target variables and run models

--- data/nlp/intro_nlp2.py
+++ data/nlp/intro_nlp2.py
@@ -13,9 +13,10 @@ import gensim
 from gensim import models, corpora
 from gensim.parsing.preprocessing import preprocess_documents
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 
 dfClean = pd.read_csv("clean_final.csv", encoding = 'unicode_escape', engine ='python')
+dfTarget = pd.read_csv("SCDB_filtered2.csv", encoding = 'unicode_escape', engine ='python')
 
 numrows = dfClean.shape[0]
 
@@ -26,7 +27,6 @@ def polarityScore(df, cols):
     for i in range(df.shape[0]):
         col0 = 0
         col1 = 0
-        print(df[cols[0]])
         if (df[cols[0]][i] != ' '):
             col0 = vader.polarity_scores(df[cols[0]][i])['compound'] 
         if (df[cols[1]][i] != ' '):
@@ -45,21 +45,46 @@ def convertCorpus(df, cols):
     tfidf_corpus = tfidf[dict_corpus]
     return tfidf_corpus
 
+def targetData(dfTarget, df):
+    target = []
+    remove_index = []
+    id = df["ID"]
+    for i in range(len(id)):
+        label = id.iloc[i]
+        if '.' in label:
+            label = label.replace('.', '')
+        res = dfTarget[dfTarget.docket == label]
+        if (res.shape[0] == 0):
+            remove_index.append(i)
+        else:
+            target.append(res.iloc[0, 1])
+    return target, remove_index
+
+# FIRST TIME THROUGH:
+
+# cols = ["Petitioner", "Respondent"]
 
 # dfOutput = pd.DataFrame(index=range(dfClean.shape[0]), columns=["Polarity"])
 # polarityCol = polarityScore(dfClean, cols)
 # dfOutput["Polarity"] = pd.Series(polarityCol)
-# dfOutput.to_csv("polarity.csv")
+# dfOutput.to_csv("polarity.csv", index=False)
 # print("finished polarity")
 
-# first time through:
-# cols = ["Petitioner", "Respondent"]
 # corpus = convertCorpus(dfClean, cols)
 # corpora.MmCorpus.serialize('./corpus.mm', corpus)
-corpus = corpora.MmCopus('./corpus.mm')
 
-# ONCE WE GET THE BINARY FOR WHICH SIDE WON
-for topic in range(50, 500, 50):
+# target, remove_index = targetData(dfTarget, dfClean)
+# dfClean.drop(remove_index, inplace=True)
+# dfClean.insert(loc=0, column='Outcome', value=target)
+# dfClean.to_csv("clean_final.csv", index=False)
+
+corpus = corpora.MmCorpus('./corpus.mm')
+actual = dfClean.Outcome
+
+logacc = []
+svmacc = []
+
+for topic in range(50, 550, 50):
     print("number of latent variables: ", topic)
     model = models.LsiModel(corpus, num_topics = topic)
     dfLsi = pd.DataFrame(columns = range(topic))
@@ -71,13 +96,41 @@ for topic in range(50, 500, 50):
     logreg.fit(dfLsi, actual)
     acc = logreg.score(dfLsi, actual)
     print("logistic accuracy: ", acc)
+    logacc.append(acc)
 
-    svm = SVC(kernel = 'linear')
+    svm = LinearSVC()
     svm.fit(dfLsi, actual)
     acc = svm.score(dfLsi, actual)
     print("support vector machine accuracy: ", acc)
+    svmacc.append(acc)
     
     print()
-    break # comment out later
+    # break # comment out later
 
-print("finished lsi")
+# choose optimal number of num_topics, the one below is current so we don't rerun
+# model = models.LsiModel(corpus, num_topics = 500)
+# dfLsi = pd.DataFrame(columns = range(500))
+# for i in range(numrows):
+    # petitioner = [item[1] for item in model[corpus[i]]]
+    # respondent = [item[1] for item in model[corpus[i + numrows]]]
+    # dfLsi.loc[len(dfLsi.index)] = [petitioner[i] - respondent[i] for i in range(len(petitioner))]
+
+logreg = LogisticRegression()
+logreg.fit(dfLsi, actual)
+acc = logreg.score(dfLsi, actual)
+print("before polarity, logistic accuracy: ", acc)
+svm = LinearSVC()
+svm.fit(dfLsi, actual)
+acc = svm.score(dfLsi, actual)
+print("before polarity, support vector machine accuracy: ", acc)
+
+polarity = dfOutput.Polarity
+dfLsi = dfLsi.assign(Polarity = polarity)
+logreg = LogisticRegression()
+logreg.fit(dfLsi, actual)
+acc = logreg.score(dfLsi, actual)
+print("after polarity, logistic accuracy: ", acc)
+svm = LinearSVC()
+svm.fit(dfLsi, actual)
+acc = svm.score(dfLsi, actual)
+print("after polarity, support vector machine accuracy: ", acc)
