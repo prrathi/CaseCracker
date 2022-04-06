c764ab5996ac83fb66c8ebda26ed137cc0a08b31
James Xie <51175490+jamesxsd19@users.noreply.github.com>
2022-04-05 22:29:01 -0500

    Added a few comments

--- frontend/src/components/pages/SurveyPage.js
+++ frontend/src/components/pages/SurveyPage.js
@@ -4,6 +4,11 @@ import { QuizButton } from '../QuizButton';
 import image from '../scotus_images.png';
 import '../SurveyPage.css';
 import MySurvey from './surveys/surveyTypeone';
+
+
+/** This is the class for the entire SurveyPage
+ * Contains the predefined SurveyJS class object
+ */
 function SurveyPage() {
   return (
     <div className='survey-container'>
