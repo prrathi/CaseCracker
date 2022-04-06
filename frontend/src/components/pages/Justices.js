961cc659923c011c125c4bdc4586b5c86e08f0ae
Sai Merneedi <smerneedi3@gmail.com>
2022-04-05 23:12:29 -0500

    Re-added previous dropdown changes.

--- frontend/src/components/pages/Justices.js
+++ frontend/src/components/pages/Justices.js
@@ -3,6 +3,12 @@ import '../../App.css';
 import { Button } from '../Button';
 import '../Justices.css';
 import styled from 'styled-components';
+import Hero from '../Hero';
+import Dropdown from 'react-bootstrap/Dropdown';
+import JustLeans from './JustLeans';
+
+  
+
 
 const JusticeButton = styled.button`
     border-radius: 5px;
@@ -16,7 +22,24 @@ const JusticeButton = styled.button`
     padding: 9px 20px;
     font-size: 17px;
     margin: 0px 0px
-    `
+`
+
+const JusticeLean = styled.button`
+background-color: #ffffff;
+color: #000000;
+outline: 0;
+border-radius: 5px;
+cursor: pointer;
+transition: all 0.3s ease-out;
+padding: 11px 26px;
+font-size: 20px;
+`
+
+const JustLean = styled.text`
+    font-size: 100px;
+    margin: 100px 100px;
+    padding-top: 100px;
+`
 
 function Justices() {
   return (
@@ -105,8 +128,17 @@ function Justices() {
                 </div>
             </div>
         </div>
+        <div>
+            <a href = '/JustLeans'>
+                <JusticeLean> Learn About How Each Justice Leans On Each Issue </JusticeLean>
+            </a>
+        </div>
+        
+        
     </div>
   );
 }
 
-export default Justices; 
+
+
+export default Justices;
