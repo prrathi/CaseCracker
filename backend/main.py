ec7181aab6f51c08a1a9ddae98be4d2d5999e01e
Aryan Nambiar <aryanjnambiar@gmail.com>
2022-04-06 17:03:37 -0500

    Added survey endpoint to receive survey data from frontend.

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/survey', methods=['POST'])
+def survey():
+    # print(request.json)
+
+    return request.json
