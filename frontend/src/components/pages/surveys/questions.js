0acd3278290408ec3321498c4d2917829ce8c810
James Xie <51175490+jamesxsd19@users.noreply.github.com>
2022-04-11 19:39:20 -0500

    Made every survey question required

--- frontend/src/components/pages/surveys/questions.js
+++ frontend/src/components/pages/surveys/questions.js
@@ -1,7 +1,6 @@
 
 export const json = 
 {
-    "completedHtml": "<h3>Thank you for your feedback.</h3> <h5>Your thoughts and ideas will help us to create a great product!</h5>",
     "title": "Political Leaning Self-Test",
     "logoPosition": "right",
     "pages": [
@@ -12,6 +11,7 @@ export const json =
         "type": "radiogroup",
         "name": "question1",
         "title": "People should have the right to bear arms.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -27,6 +27,7 @@ export const json =
         "type": "radiogroup",
         "name": "question2",
         "title": "Apple should allow law enforcement permission to go through crime suspect's phones.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -42,6 +43,7 @@ export const json =
         "type": "radiogroup",
         "name": "question3",
         "title": "The death sentence should be used.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -57,6 +59,7 @@ export const json =
         "type": "radiogroup",
         "name": "question4",
         "title": "Absentee Voting should be more accessible.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -72,6 +75,7 @@ export const json =
         "type": "radiogroup",
         "name": "question5",
         "title": "Affirmative action should be abolished.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -87,6 +91,7 @@ export const json =
         "type": "radiogroup",
         "name": "question6",
         "title": "I support LGBTQ+ rights.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -102,6 +107,7 @@ export const json =
         "type": "radiogroup",
         "name": "question7",
         "title": "School-wide prayer should be allowed.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -117,6 +123,7 @@ export const json =
         "type": "radiogroup",
         "name": "question8",
         "title": "Twitter has the right to censor speech it deems \"inappropriate or harmful",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -132,6 +139,7 @@ export const json =
         "type": "radiogroup",
         "name": "question9",
         "title": "People should have the right to protest without organization.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -147,6 +155,7 @@ export const json =
         "type": "radiogroup",
         "name": "question10",
         "title": "The government has the right to enforce mask mandates.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -162,6 +171,7 @@ export const json =
         "type": "radiogroup",
         "name": "question11",
         "title": "Should prosecutors be given the power to determine the charges a defendant is faced with?",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -177,6 +187,7 @@ export const json =
         "type": "radiogroup",
         "name": "question12",
         "title": "Cash bail be outlawed.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -192,6 +203,7 @@ export const json =
         "type": "radiogroup",
         "name": "question13",
         "title": "Abortion should be legalized nationally.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -207,6 +219,7 @@ export const json =
         "type": "radiogroup",
         "name": "question14",
         "title": "DNA databases like 23AndMe shouldn't be allowed to post data publicly.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -222,6 +235,7 @@ export const json =
         "type": "radiogroup",
         "name": "question15",
         "title": "Law enforcement has the right to sift through personal data to prevent terrorism and work in the interests of national security",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -237,6 +251,7 @@ export const json =
         "type": "radiogroup",
         "name": "question16",
         "title": "Attorneys should have a duty to inform law enforcement when their client admits to something dangerous.",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -252,6 +267,7 @@ export const json =
         "type": "radiogroup",
         "name": "question17",
         "title": "The government should have the ability to break up Big Tech",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
@@ -267,6 +283,7 @@ export const json =
         "type": "radiogroup",
         "name": "question18",
         "title": "The government should be able to increase taxes on the wealthy to use for governmental budget",
+        "isRequired": true,
         "choices": [
          {
           "value": "item1",
