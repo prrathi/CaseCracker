c68c0a7aeaa6a41d35a5dfdbdcb422c4da038110
James Xie <51175490+jamesxsd19@users.noreply.github.com>
2022-04-12 17:47:49 -0500

    Updated Results Page

--- frontend/src/components/pages/SurveyResults.js
+++ frontend/src/components/pages/SurveyResults.js
@@ -25,13 +25,22 @@ color: blue
 `
 const MostSimilar = styled.text`
 font-size: 40px;
-text-align: left;
+text-align: center;
 `
 const JusticeName = styled.text`
 font-size: 40px;
-text-align: left;
+text-align: center;
 font-weight: bolder;
-padding-left:125px;
+`
+
+const SimilarAreas = styled.text `
+font-size: 30px;
+text-align: center;
+`
+
+const similarText = styled.text `
+font-size: 30px;
+text-align: right;
 `
 
 function SurveyResult() {
@@ -46,6 +55,14 @@ function SurveyResult() {
             <div className='rightfirstJusticebox'>
                 <MostSimilar> The Justice Most Similar to You is</MostSimilar> 
                 <JusticeName> STEPHEN BREYER</JusticeName>
+                <JusticeName> (1994 - Present)</JusticeName>
+                <div className='smallLeftBox'> 
+                  <SimilarAreas> Similar Issue Areas: </SimilarAreas> 
+                  <SimilarAreas> • Criminal Procedures</SimilarAreas>
+                  <SimilarAreas> • State Rights</SimilarAreas>
+            
+                </div>
+                
             </div>
             
         </div>
