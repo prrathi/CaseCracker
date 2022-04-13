6b52272dd98444be05c516dc7e0ef41af2ac571e
Praneet Rathi <prrathi10@gmail.com>
2022-04-13 16:26:47 -0500

    progress on the transcript flask requests

--- frontend/src/components/pages/Transcript.js
+++ frontend/src/components/pages/Transcript.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import '../../App.css';
-import { Button } from '../Button';
-import image from '../scotus_images.png';
-import '../Transcript.css';
+import React from "react";
+import "../../App.css";
+import { Button } from "../Button";
+import "../Transcript.css";
 // import {Flex} from 'react-spectrum';
 
 // This is the class for the Transcript Page, where the user
@@ -14,34 +13,76 @@ class Transcript extends React.Component {
     this.handleFileClick = this.handleFileClick.bind(this);
     this.handlePetText = this.handlePetText.bind(this);
     this.handleResText = this.handleResText.bind(this);
-    this.state = {click: 0, petitionerValue: "", respondentValue: ""} //0 means nothing, 1 means text clicked, 2 means file clicked
+    this.handleResetText = this.handleResetText.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      click: 0,
+      petitionerValue: "",
+      respondentValue: "",
+      outputValue: "",
+    }; //0 means nothing, 1 means text clicked, 2 means file clicked
   }
 
   handleTextClick() {
-    this.setState({click: 1});
+    this.setState({ click: 1 });
   }
 
   handleFileClick() {
-    this.setState({click: 2});
+    this.setState({ click: 2 });
   }
 
   handlePetText(event) {
-      this.setState({petitionerValue: event.target.value});
-    }
+    this.setState({ petitionerValue: event.target.value });
+  }
 
   handleResText(event) {
-      this.setState({respondentValue: event.target.value});
-    }
+    this.setState({ respondentValue: event.target.value });
+  }
+
+  handleResetText(result) {
+    console.log("reset");
+    this.setState({
+      petitionerValue: "",
+      respondentValue: "",
+      outputValue: result,
+      click: 0
+    });
+  }
+
+  handleSubmit() {
+    console.log("here");
+    fetch('/transcript', {
+    method: "POST",
+    body: JSON.stringify({
+    respondentValue: this.state.respondentValue,
+    petitionerValue: this.state.petitionerValue
+    })
+    }).then(response => response.json().then(data => {this.handleResetText(data.result)}));
+  }
 
   render() {
     const click = this.state.click;
+    const petText = this.state.petitionerValue;
+    const resText = this.state.respondentValue;
     let media;
-    if (click == 1) {
-      media = 
-      <div> 
-        <label for="petitionerValue">Petitioners' Text:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        <label for="respondentValue">Respondents' Text:</label>
-        <br/>
+    let button;
+    let result;
+    if (click == 0) {
+      result = (
+        <div>
+          {this.state.outputValue}
+        </div>
+      )
+    }
+    else if (click === 1) {
+      media = (
+        <div>
+          <label htmlFor="petitionerValue">
+            Petitioners'
+            Text:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </label>
+          <label htmlFor="respondentValue">Respondents' Text:</label>
+          <br />
           <textarea
             id="petitionerValue"
             name="petitionerValue"
@@ -52,6 +93,7 @@ class Transcript extends React.Component {
             cols="50"
           />
           <textarea
+            id="petitionerValue"
             name="respondentValue"
             placeholder="Type respondents' text here"
             value={this.state.respondentValue}
@@ -59,67 +101,85 @@ class Transcript extends React.Component {
             rows="10"
             cols="50"
           />
-      </div>;
-    }
-    else if (click == 2) {
-      media = <div> <p>prathinav merneedi, good job on mathcounts</p> </div>;
+        </div>
+      );
+      if (petText !== "" && resText !== "") {
+        button = (
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="submit"
+                className="w3-button w3-blue"
+              />
+            </form>
+          </div>
+        );
+      }
+    } else if (click === 2) {
+      media = (
+        <div>
+          {" "}
+          <p>prathinav merneedi, good job on mathcounts</p>{" "}
+        </div>
+      );
     }
     return (
-      <div className='transcript-container'>
-        <div className='transcriptbox'>
+      <div className="transcript-container">
+        <div className="transcriptbox">
           <h1>Input Transcript</h1>
         </div>
 
-        <div className='transcriptsmallbox'>
-          <p>Input past or potential future court transcripts 
-            to determine how they would fare at the Supreme Court level. </p>
+        <div className="transcriptsmallbox">
+          <p>
+            Input past or potential future court transcripts to determine how
+            they would fare at the Supreme Court level.{" "}
+          </p>
         </div>
 
-        <div className='transcript-btns'>
-            <Button
-            className='btn'
-            buttonStyle='btn--outline'
-            buttonSize='btn--large'
+        <div className="transcript-btns">
+          <Button
+            className="btn"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
             onClick={this.handleTextClick}
-            >
-            Insert Text 
-            </Button>
-            <Button
-            className='btn'
-            buttonStyle='btn--outline'
-            buttonSize='btn--large'
+          >
+            Insert Text
+          </Button>
+          <Button
+            className="btn"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
             onClick={this.handleFileClick}
-            >
-              Upload File
-            </Button>
-          </div>
-        <div>
-          {media}
+          >
+            Upload File
+          </Button>
         </div>
+        <div>{media}</div>
+        <div>{button}</div>
       </div>
     );
   }
 }
 
 // function Media(props) {
-  // const text = props.click;
-  // if (text == 0) {
-    // return;
-  // }
-  // if (text == 1) {
-    // return(
-      // <div>
-        // <p>Petitioners' Text</p>
-        // <p>Respondents' Text</p>
-      // </div>
-    // );
-  // }
-  // if (text == 2) {
-    // // sai do your dropdown stuff here
-    // return(
-      // <p>prathinav merneedi, good job on mathcounts</p>
-    // );
-  // }
+// const text = props.click;
+// if (text == 0) {
+// return;
+// }
+// if (text == 1) {
+// return(
+// <div>
+// <p>Petitioners' Text</p>
+// <p>Respondents' Text</p>
+// </div>
+// );
+// }
+// if (text == 2) {
+// // sai do your dropdown stuff here
+// return(
+// <p>prathinav merneedi, good job on mathcounts</p>
+// );
+// }
 // }
 
 export default Transcript;
