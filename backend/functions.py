fcf311e7e99686dc5f8faf7484b784258ea58218
James Xie <51175490+jamesxsd19@users.noreply.github.com>
2022-04-15 11:48:27 -0500

    Finished functions test and added comments

--- backend/functions.py
+++ backend/functions.py
@@ -1,3 +1,4 @@
+from ast import Str
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -53,6 +54,8 @@ def distance(userAvg, targetJustice):
 
 # Function to find the two minimum distance issue areas
 def minIssueArea(userAvg, targetJustice):
+
+    # Finding the Minimum Issue Area
     issueAreas = []
     minFirst = (targetJustice[0] - userAvg[0]) * (targetJustice[0] - userAvg[0])
     issueOne = 0
@@ -61,18 +64,19 @@ def minIssueArea(userAvg, targetJustice):
             minFirst = (targetJustice[i] - userAvg[i]) * (targetJustice[i] - userAvg[i])
             issueOne = i
     
-    # copyJustice.remove(targetJustice[issueOne])
-    # copyuser.remove(userAvg[issueOne])
+
+    # Finding the second minimum issue area
+
     minSecond = (targetJustice[0] - userAvg[0]) * (targetJustice[0] - userAvg[0])
     issueSecond = 0
-    if (issueSecond == 0):
+    if (issueOne == 0):
         minSecond = (targetJustice[1] - userAvg[1]) * (targetJustice[1] - userAvg[1])
         issueSecond = 1
     for i in range(len(targetJustice)):
-        if (not math.isnan(targetJustice[i]) and ((targetJustice[i] - userAvg[i]) * (targetJustice[i] - userAvg[i]) < minSecond) and i != issueOne):
+        if ((not math.isnan(targetJustice[i]) and ((targetJustice[i] - userAvg[i]) * (targetJustice[i] - userAvg[i]) < minSecond) )and i != issueOne):
             minSecond = (targetJustice[i] - userAvg[i]) * (targetJustice[i] - userAvg[i])
-            issueSecond = i        
-    issueAreas = ["Criminal Procedure", "Civil Rights", "First Amendment", "Due Process", "Privacy", "Attorneys", "Economic Activity"]
+            issueSecond = i  
+    issueAreas = ["Criminal Procedures", "Civil Rights", "First Amendment", "Due Process", "Privacy", "Attorneys", "Economic Activity"]
     areas = [issueAreas[issueOne], issueAreas[issueSecond]]
     return areas
     
@@ -83,18 +87,23 @@ def minIssueArea(userAvg, targetJustice):
 # justices is a 2d list with each row corresponding to each justice
 # return type: int index representing the ID of the closest justice
 def findClosestJustice(userAvg):
+    # Getting the distance between user and each justice
     distances = []
     for i in range(len(df1)):
         distances.append(distance(userAvg, df1[i]))
+
+
+    # Finding the first judge that is closest
     minFirst = distances[0]
     minJudgeFirst = 0
     for i in range(len(distances)):
         if ((distances[i] != -1 and distances[i] < minFirst)):
             minFirst = distances[i]
             minJudgeFirst = i
-    # distances.remove(minFirst)
+    # Finding the two closest issue areas for the first judge
     minIssueAreaOne = minIssueArea(userAvg, df1[minJudgeFirst])
 
+    # Second judge
     minSecond = distances[0]
     minJudgeSecond = 0
     if (minJudgeFirst == 0):
@@ -106,8 +115,7 @@ def findClosestJustice(userAvg):
             minJudgeSecond = i
     minIssueAreaTwo = minIssueArea(userAvg, df1[minJudgeSecond])
 
-
-    # distances.remove(minSecond)
+    # Third Judge
     minThird = distances[0]
     minJudgeThird = 0
     if ((minJudgeFirst == 0 and minJudgeSecond == 1) or (minJudgeSecond == 0 and minJudgeFirst == 1)):
@@ -122,9 +130,10 @@ def findClosestJustice(userAvg):
             minThird = distances[i]
             minJudgeThird = i
     
+    # Formatting output of function
     minIssueAreThree = minIssueArea(userAvg, df1[minJudgeThird])
     judgeOne = [justice_names[minJudgeFirst], minIssueAreaOne[0], minIssueAreaOne[1]]
-    judgeTwo = [justice_names[minJudgeFirst], minIssueAreaTwo[0], minIssueAreaTwo[1]]
+    judgeTwo = [justice_names[minJudgeSecond], minIssueAreaTwo[0], minIssueAreaTwo[1]]
     judgeThree = [justice_names[minJudgeThird], minIssueAreThree[0], minIssueAreThree[1]]
     minJudge = [judgeOne, judgeTwo, judgeThree]
     return minJudge
@@ -133,13 +142,7 @@ def findClosestJustice(userAvg):
 
 # Example method to use main
 def main():
-    # Import Dataframe
-    # df = pd.read_csv("/Users/jamesxie/Desktop/course-project-kk-b/data/user_classification/justice_leanings.csv")
-    
-    # df.drop(columns=df.columns[0],  
-    #     axis=1, 
-    #     inplace=True)
-    selfResponses = [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1]
+    selfResponses = [1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1]
     issueAverages = findIssueAvg(selfResponses)
     closestJustice = findClosestJustice(issueAverages)
     print(closestJustice)
