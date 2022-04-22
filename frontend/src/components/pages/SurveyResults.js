eaea7d7b1e825c03ef335d49578bba3ec71cd2b1
James Xie <51175490+jamesxsd19@users.noreply.github.com>
2022-04-22 14:50:38 -0500

    Adjusted Images to match survey results

--- frontend/src/components/pages/SurveyResults.js
+++ frontend/src/components/pages/SurveyResults.js
@@ -56,16 +56,65 @@ color: #ffffff;
     button-align: center;
 
 `
-var data = JSON.parse(sessionStorage.getItem("first"))["justices"];
+var data = [[0,1,2],[1,2,3],[4,5,6]];
+if (sessionStorage.getItem("first") == 0) {
+  data = [[0,1,2],[1,2,3],[4,5,6]];
+} else {
+  data = JSON.parse(sessionStorage.getItem("first"))["justices"];
+}
+
+var options = new Map([
+  [  'Hugo Black', "./images/hugo_black_highres.png" ],
+  [  'Stanley Reed', "./images/stanley_reed_highres.png" ],
+  [  'Felix Frankfurter', "./images/felix_frankfurter_highres.png"  ],
+  [  'William Douglas', "./images/william_douglas_highres.png" ],
+  [  'Francis Murphy', "./images/francis_murphy_highres.png"  ],
+  [  'James Byrnes', "./images/james_brynes_highres.png" ],
+  [  'Robert Jackson', "./images/robert_jackson_highres.png"  ],
+  [  'Wiley Rutledge', "./images/wiley_rutledge_highres.png" ],
+  [  'Harold Burton', "./images/harold_burton_highres.png" ],
+  [  'Fred Vinson', "./images/fred_vinson_highres.png" ],
+  [  'Tom Clark', "./images/tom_clark_highres.png" ],
+  [  'Sherman Minton', "./images/sherman_minton_highres.png" ],
+  [  'Earl Warren', "./images/earl_warren_highres.png" ],
+  [  'John Harlan', "./images/john_harlan_highres.png" ],
+  [  'William Brennan', "./images/william_brennan_highres.png" ],
+  [  'Charles Whittaker', "./images/charles_whittaker_highres.png" ],
+  [  'Potter Stewart', "./images/potter_stewart_highres.png" ],
+  [  'Byron White', "./images/byron_white_highres.png" ],
+  [  'Arthur Goldberg', "./images/arthur_goldberg_highres.png" ],
+  [  'Abe Fortas', "./images/abe_fortas_highres.png" ],
+  [  'Thurgood Marshall', "./images/thurgood_marshall_highres.png" ],
+  [  'Warren Burger', "./images/warren_burger_highres.png" ],
+  [  'Harry Blackmun', "./images/harry_blackmun_highres.png" ],
+  [  'Lewis Powell', "./images/lewis_powell_highres.png" ],
+  [  'William Rehnquist', "./images/william_rehnquist_highres.png" ],
+  [  'John Stevens', "./images/john_stevens_highres.png" ],
+  [  'Sandra O\'Connor', "./images/sandra_o_connor_highres.png" ],
+  [  'Antonin Scalia', "./images/antonin_scalia_highres.png" ],
+  [  'Anthony Kennedy', "./images/anthony_kennedy_highres.png" ],
+  [  'David Souter', "./images/david_souter_highres.png" ],
+  [  'Clarence Thomas', "./images/clarence_thomas_highres.png" ],
+  [  'Ruth Bader Ginsburg', "./images/ruth_bader_ginsburg.png" ],
+  [  'Stephen Breyer', "./images/stephen_breyer_highres.png" ],
+  [  'John Roberts', "./images/john_roberts_highres.png" ],
+  [  'Samuel Alito', "./images/samuel_alito_highres.png" ],
+  [  'Sonia Sotomayor', "./images/sonia_sotomayor_highres.png" ],
+  [  'Elena Kagan', "./images/elena_kagan_highres.png" ],
+  [  'Neil Corsuch', "./images/neil_corsuch_highres.png" ],
+  [  'Brett Kavanaugh', "./images/brett_kavanuagh_highres.png" ],
+  [  'Amy Coney Barrett', "./images/amy_coney_barrett.png" ]]);
+ 
 
 function SurveyResult() {
+  
   return (
     <div className='surveyresults-container'>
         <JustLean>Based on your responses, you appear to lean</JustLean>
         <Lean> Liberal </Lean>
         <div className='firstjusticebox'> 
             <div className='leftfirstJusticebox'>
-                <img src="./images/stephen_breyer_highres.png" height={400} width={325} />
+                <img src={options.get(data[0][0])} height={400} width={325} />
             </div>
             <div className='rightfirstJusticebox'>
                 <MostSimilar> The Justice Most Similar to You is</MostSimilar> 
@@ -92,7 +141,7 @@ function SurveyResult() {
         <JustLean> ALSO SIMILAR TO YOU</JustLean> 
         <div className='firstjusticebox'> 
             <div className='leftfirstJusticebox'>
-                <img src="./images/ruth_bader_ginsburg.png" height={400} width={325} />
+                <img src={options.get(data[1][0])} height={400} width={325} />
             </div>
             <div className='rightfirstJusticebox'>
                 <JusticeName> {data[1][0]} </JusticeName>
@@ -100,7 +149,7 @@ function SurveyResult() {
                   <div className='smallLeftBox'> 
                     <SimilarAreas> Similar Issue Areas: </SimilarAreas> 
                     <SimilarText> • {data[1][1]} </SimilarText>
-                    <SimilarText> • {data[1][2]} </SimilarText>
+                    <SimilarText> • {data[1][2]}</SimilarText>
               
                   </div>
                   <div className='smallRightBox'>
@@ -117,7 +166,7 @@ function SurveyResult() {
         </div>
         <div className='firstjusticebox'> 
             <div className='leftfirstJusticebox'>
-                <img src="./images/sonia_sotomayor_highres.png" height={400} width={325} />
+                <img src={options.get(data[2][0])} height={400} width={325} />
             </div>
             <div className='rightfirstJusticebox'>
                 <JusticeName> {data[2][0]} </JusticeName>
